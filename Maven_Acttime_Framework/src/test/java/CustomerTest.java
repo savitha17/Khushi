fae5f30822e5e7aa6c3159326edaf89319e910a5
Savitha <Savitha@Keerthi-PC>
2019-05-10 10:40:29 +0530

    MyMavenframework

---
+++ Maven_Acttime_Framework/src/test/java/CustomerTest.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+@Test
+public class CustomerTest {
+	public void createcustomer() {
+		System.out.println("createcustomer");
+	}
+public void modifycustomer() {
+	System.out.println("modifycustomer");
+	}
+public void deletecustomer() {
+	System.out.println("deletecustomer");
+}
+
+
+}
